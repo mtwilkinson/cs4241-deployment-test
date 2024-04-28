356a1b2bdd019f5d78cee46f89bedca43476d613
mtwilkinson <122630959+mtwilkinson@users.noreply.github.com>
2024-04-28 18:52:41 -0400

    making navbar update with when logged in

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 function NavBar() {
+
+    const [loggedIn, setLoggedIn] = useState(false)
+
+
+    useEffect(() => {
+        const interval = setInterval(() => setLoggedIn(localStorage.getItem("accessToken") === null), 1000);
+        return () => {
+            clearInterval(interval);
+        };
+    }, []);
 
     return (
         <>
             {
-                localStorage.getItem("accessToken") === null ? (
-                    <div className={"h-16 flex px-6 items-center w-screen bg-green-700 gap-4"} role="navigation" aria-label="Main navigation" >
+                !loggedIn ? (
+                    <div className={"min-h-16 flex px-6 items-center w-screen bg-green-700 gap-4"} role="navigation" aria-label="Main navigation" >
                         <a className="font-mono font-bold text-2xl text-white hover:text-emerald-300"
                            href={"/"} title="Go to login page">Login</a>
                         <a className="font-mono font-bold text-2xl text-white hover:text-emerald-300"
                            href={"/instructions"} title="Go to instructions page">Instructions</a>
                     </div>
                 ) : (
-                    <div className={"h-16 flex px-6 items-center w-screen bg-green-700 gap-4"} role="navigation" aria-label="Main navigation" >
+                    <div className={"min-h-16 flex px-6 items-center w-screen bg-green-700 gap-4"} role="navigation" aria-label="Main navigation" >
                         <a className="font-mono font-bold text-2xl text-white hover:text-emerald-300"
                            href={"/login"} title="Return to homepage">Homepage</a>
                         <a className=" font-mono font-bold text-2xl text-white hover:text-emerald-300"
